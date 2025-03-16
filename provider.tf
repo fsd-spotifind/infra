650d12ab526df90dbdc7a8e43337ce6828ce48f6
nicamanas <xyukimiku@gmail.com>
2025-03-16 23:40:11 +0800

    reference variables

--- provider.tf
+++ provider.tf
@@ -9,7 +9,10 @@ terraform {
 
 provider "aws" {
   region  = "ap-southeast-1"
-  access_key = env("AWS_ACCESS_KEY_ID")
-  secret_key = env("AWS_SECRET_ACCESS_KEY")
-  token      = env("AWS_SESSION_TOKEN")
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  token      = var.AWS_SESSION_TOKEN
+
+#   # You can also set `skip_credentials_validation` to true to prevent Terraform from trying to validate credentials.
+#   skip_credentials_validation = true
 }
