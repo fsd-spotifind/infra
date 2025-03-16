0cc077795380375895fde64f40b8bb6adbb0bc70
nicamanas <xyukimiku@gmail.com>
2025-03-16 23:38:52 +0800

    reference env variables from oidc

--- provider.tf
+++ provider.tf
@@ -8,5 +8,8 @@ terraform {
 }
 
 provider "aws" {
-    region = "ap-southeast-1"
+  region  = "ap-southeast-1"
+  access_key = env("AWS_ACCESS_KEY_ID")
+  secret_key = env("AWS_SECRET_ACCESS_KEY")
+  token      = env("AWS_SESSION_TOKEN")
 }
