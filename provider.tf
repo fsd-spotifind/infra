1ed8079c174f77e63f12e9189b13cfb45ca17a3e
nicamanas <xyukimiku@gmail.com>
2025-03-16 23:42:37 +0800

    idk anymore

--- provider.tf
+++ provider.tf
@@ -9,10 +9,4 @@ terraform {
 
 provider "aws" {
   region  = "ap-southeast-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  token      = var.AWS_SESSION_TOKEN
-
-#   # You can also set `skip_credentials_validation` to true to prevent Terraform from trying to validate credentials.
-#   skip_credentials_validation = true
 }
