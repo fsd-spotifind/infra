a2eda84b6ad6f213efafcdb928291d81202f9de9
nicamanas <xyukimiku@gmail.com>
2025-03-24 10:51:35 +0800

    fix character edit

--- vpc.tf
+++ vpc.tf
@@ -41,5 +41,5 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table_association" "public" {
     subnet_id = aws_subnet.public.id
-    route_table_id = aws_route_table.public.idß
+    route_table_id = aws_route_table.public.id
 }
