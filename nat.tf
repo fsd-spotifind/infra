ddc08ebf96ffe8608a0ddaeb4d74a77d25a9d62d
nicamanas <xyukimiku@gmail.com>
2025-03-24 12:34:50 +0800

    remove update route tables

--- nat.tf
+++ nat.tf
@@ -6,8 +6,8 @@ module "fck-nat" {
     subnet_id = aws_subnet.public.id
     use_cloudwatch_agent = true
 
-    update_route_tables = true
-    route_tables_ids = {
-        "spotifind-public-rt" = aws_route_table.public.id
-    }
+    update_route_tables = false
+    # route_tables_ids = {
+    #     "spotifind-public-rt" = aws_route_table.public.id
+    # }
 }
