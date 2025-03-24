074ee8bc7850a74ad41c925e70de9b17db207c68
nicamanas <xyukimiku@gmail.com>
2025-03-24 17:22:19 +0800

    change subnet configs

--- vpc.tf
+++ vpc.tf
@@ -17,7 +17,7 @@ resource "aws_internet_gateway" "igw" {
 # for our nat instance
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.18.1.0/24"
+  cidr_block              = "172.18.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-southeast-1a"
 
@@ -28,7 +28,7 @@ resource "aws_subnet" "public_a" {
 
 resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.18.2.0/24"
+  cidr_block              = "172.18.3.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-southeast-1a"
 
@@ -62,7 +62,7 @@ resource "aws_route_table_association" "public_b" {
 
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "172.18.3.0/24"
+  cidr_block        = "172.19.2.0/24"
   availability_zone = "ap-southeast-1a"
 
   tags = {
@@ -72,7 +72,7 @@ resource "aws_subnet" "private_a" {
 
 resource "aws_subnet" "private_b" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "172.18.4.0/24"
+  cidr_block        = "172.19.3.0/24"
   availability_zone = "ap-southeast-1b"
 
   tags = {
