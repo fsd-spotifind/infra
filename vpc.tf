337b2f5539f5e34861c558ecd04906844a2abbb8
nicamanas <xyukimiku@gmail.com>
2025-03-24 15:03:57 +0800

    add missing rt refs

--- vpc.tf
+++ vpc.tf
@@ -98,12 +98,12 @@ resource "aws_route_table" "private_b" {
 
 resource "aws_route_table_association" "private_a" {
   subnet_id      = aws_subnet.private_a.id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private_a.id
 }
 
 resource "aws_route_table_association" "private_b" {
   subnet_id      = aws_subnet.private_b.id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private_b.id
 }
 
 resource "aws_security_group" "ecs_sg" {
