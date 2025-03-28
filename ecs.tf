228a940513f21dffe149d56c81329fb05ed11e63
nicamanas <xyukimiku@gmail.com>
2025-03-28 23:51:28 +0800

    reduce ecs cpu config

--- ecs.tf
+++ ecs.tf
@@ -27,7 +27,7 @@ resource "aws_ecs_task_definition" "ecs_task" {
   family                   = "spotifind-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
+  cpu                      = "256"
   memory                   = "1024"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
